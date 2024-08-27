8197a3f31018e36f5c613ebb91537ad00c3a274d
mk <33239408+WhaleMountain@users.noreply.github.com>
2024-08-27 12:03:21 +0900

    verup

--- app/src/main/java/protobufhandler/App.java
+++ app/src/main/java/protobufhandler/App.java
@@ -12,7 +12,7 @@ import protobufhandler.view.MainView;
 public class App implements BurpExtension {
     private final String extensionName = "Protobuf Handler";
     private final String editorCaption = "Protobuf to Json Decode";
-    private final String extensionVersion = "v0.0.5";
+    private final String extensionVersion = "v0.0.6";
 
     @Override
     public void initialize(MontoyaApi api) {
