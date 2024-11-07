e14671f54781380a0f2ccb07d04eafe5cc3e1ca0
mk <33239408+WhaleMountain@users.noreply.github.com>
2024-11-07 16:42:04 +0900

    update example

--- example/client-go/main.go
+++ example/client-go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 
@@ -12,14 +13,57 @@ import (
 )
 
 func main() {
-	PROXY_URL := "http://127.0.0.1:8090"
-	REQUEST_METHOD := "POST"
-	REQUEST_URL := "http://localhost:8000/user"
+	sendHello()
+	//sendUser()
+}
 
+func sendHello() {
+	const (
+		PROXY_URL      = "http://127.0.0.1:8090"
+		REQUEST_METHOD = "POST"
+		REQUEST_URL    = "http://localhost:8000/hello" // /user
+	)
+
+	body := &pb.HelloRequest{
+		Name: "Taro",
+	}
+
+	protobufBody, _ := proto.Marshal(body)
+	request, _ := http.NewRequest(REQUEST_METHOD, REQUEST_URL, bytes.NewReader(protobufBody))
+	request.Header.Set("Content-Type", "application/x-protobuf")
+
+	proxy, _ := url.Parse(PROXY_URL)
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxy),
+		},
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	respMessage := &pb.HelloReply{}
+	respBody, _ := io.ReadAll(response.Body)
+	proto.Unmarshal(respBody, respMessage)
+
+	fmt.Println("Resp status", response.Status)
+	fmt.Println("Resp message", respMessage)
+}
+
+func sendUser() {
+	const (
+		PROXY_URL      = "http://127.0.0.1:8090"
+		REQUEST_METHOD = "POST"
+		REQUEST_URL    = "http://localhost:8000/user"
+	)
+
+	// /user request body
 	body := &pb.PostUserRequest{
 		User: &pb.User{
-			Id: "1",
-			Name: "Tanakaz",
+			Id:    "1",
+			Name:  "Tanakaz",
 			Email: "tanakaz@example.com",
 		},
 	}
@@ -40,5 +84,10 @@ func main() {
 	}
 	defer response.Body.Close()
 
+	respMessage := &pb.HelloReply{}
+	respBody, _ := io.ReadAll(response.Body)
+	proto.Unmarshal(respBody, respMessage)
+
 	fmt.Println("Resp status", response.Status)
+	fmt.Println("Resp message", respMessage)
 }
